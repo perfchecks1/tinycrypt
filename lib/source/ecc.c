e1e679fa4ab1e449377682023812aac2fe940ac3
Fabien Lheureux <fabien.lheureux@trust-in-soft.com>
2020-02-18 16:43:43 +0100

    Fix pointer arithmetic inside object, computing &array[-1]

--- lib/source/ecc.c
+++ lib/source/ecc.c
@@ -235,7 +235,8 @@ static void uECC_vli_rshift1(uECC_word_t *vli, wordcount_t num_words)
 	uECC_word_t carry = 0;
 
 	vli += num_words;
-	while (vli-- > end) {
+	while (vli > end) {
+		vli--;
 		uECC_word_t temp = *vli;
 		*vli = (temp >> 1) | carry;
 		carry = temp << (uECC_WORD_BITS - 1);
